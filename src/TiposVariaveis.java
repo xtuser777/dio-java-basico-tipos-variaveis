60bd1095b33b664ca4392cdcab24cc55da4b955b
LUCAS S. OLIVEIRA <lucaoxt@gmail.com>
2024-02-14 14:44:00 -0300

    aula2

--- src/TiposVariaveis.java
+++ src/TiposVariaveis.java
@@ -1,5 +1,14 @@
 public class TiposVariaveis {
     public static void main(String[] args) {
-        
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333;
+        long cpf = 45345345345345L;
+        float PI = 3.14F;
+        double salario = 1234.65;
+
+        short numeroCurto = 1;
+        int numberoNormal = numeroCurto;
+        short numeroCurto2 = (short) numberoNormal;
     }
 }
