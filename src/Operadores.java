684f2616b4f91116bea901174fb07dfedab2d2cf
LUCAS S. OLIVEIRA <lucaoxt@gmail.com>
2024-02-14 15:35:58 -0300

    operadores2

---
+++ src/Operadores.java
@@ -0,0 +1,15 @@
+public class Operadores {
+    public static void main(String[] args) {
+        String concat = "";
+
+        concat = 1+1+1+"1";
+
+        concat = 1+"1"+1+1;
+
+        concat = 1+"1"+1+"1";
+        
+        concat = "1"+1+1+1;
+
+        concat = "1"+(1+1+1);
+    }
+}
